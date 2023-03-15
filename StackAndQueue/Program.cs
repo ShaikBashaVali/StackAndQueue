2e0a13e07e19a516c819a8e6c79424b0edbab113
BashaVali <bashavali222@gmail.com>
2023-03-15 08:00:19 +0530

    Stack Linked List Adding Elements

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StackAndQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Stack and Queue Linked List");
+            StackLinkedList stackLinkedList = new StackLinkedList();
+            stackLinkedList.Push(70);
+            stackLinkedList.Push(30);
+            stackLinkedList.Push(56);
+            stackLinkedList.Display();
+        }
+    }
+}
